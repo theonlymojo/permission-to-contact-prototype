038d348774758ba5980b97f881f0a76c1ff8156f
davidhunter08 <thebeakking@gmail.com>
2020-06-17 11:48:05 +0100

    add v1 pages

--- app/filters.js
+++ app/filters.js
@@ -7,39 +7,59 @@ module.exports = function (env) {
    */
   var filters = {}
 
-  /* ------------------------------------------------------------------
-    add your methods to the filters obj below this comment block:
-    @example:
+    filters.toArray = val => typeof val === 'string' ? [val] : val;
 
-    filters.sayHi = function(name) {
-        return 'Hi ' + name + '!'
+  // Filter to change a number, like 1 "12" or 04, to a month in words
+  //
+  // Accepts numbers, strings, data[] and variables
+  // If empty prints nothing unless you give it a placeholder which it will print out
+  //
+  // Usage in your templates:
+  //
+  // With number with filer
+  //                    {{ 1 | monthToWord }}
+  // With data from previous input
+  //                    {{ data['dob-month'] | monthToWord }}
+  // With data that's empty with alternative placeholder
+  //                    {{ data['dob-month'] | monthToWord('January') }}
+
+  filters.monthToWord = function(month,placeholder) {
+    var months = {
+      "1":'January',
+      "01":'January',
+      "2":'February',
+      "02":'February',
+      "3":'March',
+      "03":'March',
+      "4":'April',
+      "04":'April',
+      "5":'May',
+      "05":'May',
+      "6":'June',
+      "06":'June',
+      "7":'July',
+      "07":'July',
+      "8":'August',
+      "08":'August',
+      "9":'September',
+      "09":'September',
+      "10":'October',
+      "11":'November',
+      "12":'December'
     }
 
-    Which in your templates would be used as:
+    if (month) {
 
-    {{ 'Paul' | sayHi }} => 'Hi Paul'
+      month.toString()
+      return months[month]
 
-    Notice the first argument of your filters method is whatever
-    gets 'piped' via '|' to the filter.
-
-    Filters can take additional arguments, for example:
-
-    filters.sayHi = function(name,tone) {
-      return (tone == 'formal' ? 'Greetings' : 'Hi') + ' ' + name + '!'
+    } else if (placeholder) {
+      return placeholder
+    } else {
+      return
     }
 
-    Which would be used like this:
+  }
 
-    {{ 'Joel' | sayHi('formal') }} => 'Greetings Joel!'
-    {{ 'Gemma' | sayHi }} => 'Hi Gemma!'
-
-    For more on filters and how to write them see the Nunjucks
-    documentation.
-
-  ------------------------------------------------------------------ */
-
-  /* ------------------------------------------------------------------
-    keep the following line to return your filters to the app
-  ------------------------------------------------------------------ */
   return filters
 }
--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,24 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// Nation routing - V1
+router.post('/v1/name', function (req, res) {
+
+  var install = req.session.data['nation']
+
+  if (install == "Scotland"){
+    res.redirect('/nation-redirect')
+  }
+  if (install == "Wales"){
+    res.redirect('/nation-redirect')
+  }
+  if (install == "Northern Ireland"){
+    res.redirect('/nation-redirect')
+  }
+  else {
+    res.redirect('/v1/name')
+  }
+
+})
+
 module.exports = router;
