522daa04c865b54ffe01c3c12d77d62b82989c99
Henry Cookson <henry.cookson@rocketmail.com>
2020-06-17 17:51:53 +0100

    remove original v1 from index, add v1.1 and change v2 to v1.2

--- app/routes.js
+++ app/routes.js
@@ -4,36 +4,50 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
-// Nation routing - V1
-router.post('/v1/name', function (req, res) {
+// Nation routing - V1.1
+router.post('/v1-1/postcode', function (req, res) {
 
-  var install = req.session.data['nation']
+  var install = req.session.data['country']
 
   if (install == "Scotland"){
-    res.redirect('/v1/nation-redirect')
+    res.redirect('/v1-1/nation-redirect')
   }
   if (install == "Wales"){
-    res.redirect('/v1/nation-redirect')
+    res.redirect('/v1-1/nation-redirect')
   }
   if (install == "Northern Ireland"){
-    res.redirect('/v1/nation-redirect')
+    res.redirect('/v1-1/nation-redirect')
   }
   else {
-    res.redirect('/v1/name')
+    res.redirect('/v1-1/postcode')
   }
 
 })
 
-// Tested routing - V2
-router.post('/v2/test-results', function (req, res) {
+// Tested routing - V1.1
+router.post('/v1-1/test-results', function (req, res) {
 
   var install = req.session.data['tested']
 
   if (install == "Yes"){
-    res.redirect('/v2/test-results')
+    res.redirect('/v1-1/test-results')
   }
   else {
-    res.redirect('/v2/check-answers')
+    res.redirect('/v1-1/check-answers')
+  }
+
+})
+
+// Tested routing - V1.2
+router.post('/v1-2/test-results', function (req, res) {
+
+  var install = req.session.data['tested']
+
+  if (install == "Yes"){
+    res.redirect('/v1-2/test-results')
+  }
+  else {
+    res.redirect('/v1-2/check-answers')
   }
 
 })
