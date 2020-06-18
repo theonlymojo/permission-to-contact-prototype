13ebbdb968dce1ef9be4429e69c9712cc0b27380
Henry Cookson <henry.cookson@rocketmail.com>
2020-06-18 11:25:09 +0100

    add NI postcode or town screens

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
-// Nation routing - V1.1
+// Country routing - V1.1
 router.post('/v1-1/postcode', function (req, res) {
 
   var install = req.session.data['country']
@@ -76,6 +76,20 @@ router.post('/v1-2/test-results', function (req, res) {
   }
   else {
     res.redirect('/v1-2/check-answers')
+  }
+
+})
+
+// Northern Ireland routing - V1.2
+router.post('/v1-2/postcode', function (req, res) {
+
+  var install = req.session.data['country']
+
+  if (install == "Northern Ireland"){
+    res.redirect('/v1-2/NI-postcode')
+  }
+  else {
+    res.redirect('/v1-2/postcode')
   }
 
 })
