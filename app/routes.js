694ae55131d34aecdbe22c52a1749938b1832e00
Henry Cookson <henry.cookson@rocketmail.com>
2020-06-19 18:36:18 +0100

    add version 2 for user flow 3 on Miro - inlcuding pregnancy, sex, ethnicity, health problems and re-worked work/job questions in addition to content changes suggested by Jack on the permission and contact from NHS D pages. Also updated check your answers page

--- app/routes.js
+++ app/routes.js
@@ -126,6 +126,106 @@ router.post('/v1-2/hear-from-nhsd', function (req, res) {
 
 })
 
+
+// Over 18 routing from DOB - V2
+router.post('/v2/are-you-pregnant', function (req, res) {
+
+  var install = req.session.data['dob-year']
+
+  if (install > "2002"){
+    res.redirect('/v2/under18')
+  }
+  else {
+    res.redirect('/v2/are-you-pregnant')
+  }
+
+})
+
+// Pregnant routing - V2
+router.post('/v2/name', function (req, res) {
+
+  var install = req.session.data['areyoupregnant']
+
+  if (install == "Yes"){
+    res.redirect('/v2/pregnant')
+  }
+  else {
+    res.redirect('/v2/name')
+  }
+
+})
+
+// Tested routing - V2
+router.post('/v2/test-results', function (req, res) {
+
+  var install = req.session.data['tested']
+
+  if (install == "Yes"){
+    res.redirect('/v2/test-results')
+  }
+  else {
+    res.redirect('/v2/health-problems')
+  }
+
+})
+
+// Northern Ireland routing - V2
+router.post('/v2/postcode', function (req, res) {
+
+  var install = req.session.data['country']
+
+  if (install == "Northern Ireland"){
+    res.redirect('/v2/NI-postcode')
+  }
+  if (install == "None of these"){
+    res.redirect('/v2/outside-uk')
+  }
+  else {
+    res.redirect('/v2/postcode')
+  }
+
+})
+
+// Permission routing - V2
+router.post('/v2/hear-from-nhsd', function (req, res) {
+
+  var install = req.session.data['give-permission']
+
+  if (install == "No, I do not agree to give my permission"){
+    res.redirect('/v2/no-permission')
+  }
+  else {
+    res.redirect('/v2/hear-from-nhsd')
+  }
+
+})
+
+// Ethnicity question routing - V2
+router.post('/v2/email', function (req, res) {
+
+  var install = req.session.data['ethnic-group']
+
+  if (install == "White"){
+    res.redirect('/v2/ethnic-group-white')
+  }
+  if (install == "Mixed or multiple ethnic groups"){
+    res.redirect('/v2/ethnic-group-mixed')
+  }
+  if (install == "Asian or Asian British"){
+    res.redirect('/v2/ethnic-group-asian')
+  }
+  if (install == "Black, African, Black British or Caribbean"){
+    res.redirect('/v2/ethnic-group-black')
+  }
+  if (install == "Another ethnic group"){
+    res.redirect('/v2/ethnic-group-other')
+  }
+  else {
+    res.redirect('/v2/email')
+  }
+
+})
+
 // Opt out confirm email - v1
 router.post('/opt-out-v1/opt-out-confirmation', function (req, res) {
 
