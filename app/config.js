aa1f9a797c2c031e8d8c16942e41e2713e56ae0e
davidhunter08 <thebeakking@gmail.com>
2020-07-01 09:34:49 +0100

    update changes based on stakeholder run through

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Sign up to be contacted for coronavirus vaccine research',
+  serviceName: 'Sign up to be contacted for coronavirus vaccine studies',
 
   // Port to run nodemon on locally
   port: 2000,
